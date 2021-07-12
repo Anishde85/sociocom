087b337b2b7cae4248877ecdac70f7fe66608fce
Manish Kundu <manishkundu@live.com>
2021-07-12 14:55:18 +0530

    Added login required to chat rooms.

--- chat/views.py
+++ chat/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
-
+@login_required
 def index(request):
     return render(request, 'index.html', {})
 
-
+@login_required
 def room(request, room_name):
     return render(request, 'chatroom.html', {
         'room_name': room_name
