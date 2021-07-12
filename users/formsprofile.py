6dc1945bfa6cbcd97f586f1391ef9aed2161e586
Manish Kundu <manishkundu@live.com>
2021-07-12 14:29:11 +0530

    Modified update profile.

--- users/formsprofile.py
+++ users/formsprofile.py
@@ -1,7 +1,6 @@
 from django import forms
 class UpdateForm(forms.Form):
-    username=forms.CharField(max_length=30, required=True)
-    email = forms.EmailField(required=True)
+    confirm_email = forms.EmailField(required=True)
     location = forms.CharField(max_length=30, required=False)
     about_me = forms.CharField(required=False)
     image = forms.ImageField(required=False)
