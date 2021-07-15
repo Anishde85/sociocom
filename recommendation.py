a9cc7df21a53c1771f561e50ffff342cdbeb5a05
Manish Kundu <manishkundu@live.com>
2021-07-15 22:05:42 +0530

    Updated recommendations.

--- recommendation.py
+++ recommendation.py
@@ -5,6 +5,19 @@ Created on Wed Jul 14 02:00:00 2021
 @author: me_so
 """
 import csv
+from collections import Counter
+
+def get_recommendations(text):
+    words = ["".join(k for k in word if k.isalpha() or k.isdigit()) for word in text.split()]
+    words_to_exclude = ["the", "is", "in", "of", "on", "to", "with", "he", "they", "them", "it"]
+    results = set()
+    for word, count in Counter(words).most_common():
+        if word in words_to_exclude: continue
+        for result in recommendation(word):
+            results.add(result)
+        if len(results) >= 5: break
+    return list(results)[:5]
+    
 
 def recommendation(key):
 
@@ -23,11 +36,11 @@ def recommendation(key):
     for product in data[1:]:
         ok=0
         for j in check:
-            if key in product[j]:
+            if key.lower() in product[j].lower():
                 ok=1
                 break
         if ok:
-            url.append([product[18],product[15]])
+            url.append((product[18],product[15]))
             c+=1
         if c==5:
             break
