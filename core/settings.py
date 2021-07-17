84e5c3bf4907ffcb2ecfa836d0577e95428be6a1
anishde85 <anishde85@gmail.com>
2021-07-17 10:06:38 +0530

    static removed

--- core/settings.py
+++ core/settings.py
@@ -127,14 +127,7 @@ ALLOWED_HOSTS = ['*']
 TEMPLATE_DIRS = (
     os.path.join(BASE_DIR, 'templates'),
 )
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),  # Here you tell django to look for a folder named 'assets'
-]
-STATIC_URL = '/static/'
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 
 # Path where media is stored
